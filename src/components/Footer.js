34a9e4233c52f743e19db21391ad63009229cd8f
Kenneth Martin <developer@kennethmartin.me>
2020-11-25 23:19:04 -0500

    chore: resolve stylelint error

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ import Content from 'src/components/Content';
 const Footer = styled.footer`
   background-color: ${(props) => props.theme.colors[200]};
   height: 100px;
-  dpadding: 1em 0;
+  padding: 1em 0;
 `;
 
 const _Footer = () => (
