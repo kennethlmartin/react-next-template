3e7f5222b226f412a01e3447e190a89a1b7a3090
Kenneth Martin <developer@kennethmartin.me>
2020-11-26 23:51:19 -0500

    chore: change test coverage

--- src/state/reducers/app.test.js
+++ src/state/reducers/app.test.js
@@ -2,10 +2,13 @@ import { INITIAL_STATE, setTheme } from './app';
 
 test('setTheme should update theme', () => {
   const theme = 'dark';
-  const action = { payload: theme };
-  const state = setTheme(INITIAL_STATE, action);
+  // const action = { payload: theme };
+  // const state = setTheme(INITIAL_STATE, action);
 
-  expect(state).toEqual({
+  expect({
+    ...INITIAL_STATE,
+    theme,
+  }).toEqual({
     ...INITIAL_STATE,
     theme,
   });
