b3944712504f68e08b22be682fa48c89481c5a9c
Kenneth Martin <developer@kennethmartin.me>
2021-10-10 14:02:12 -0400

    feat: remove redux sagas

--- src/state/rootSaga.js
+++
@@ -1,6 +0,0 @@
-import { all, fork } from 'redux-saga/effects';
-import { watchTestSaga } from './sagas/watchTestSaga';
-
-export const rootSaga = function* rootSaga() {
-  yield all([fork(watchTestSaga)]);
-};
--- src/state/store.js
+++ src/state/store.js
@@ -1,21 +1,8 @@
-import createSagaMiddleware from 'redux-saga';
 import { createWrapper } from 'next-redux-wrapper';
 import { configureStore } from '@reduxjs/toolkit';
 
 import { rootReducer } from 'src/state/rootReducer';
-import { rootSaga } from 'src/state/rootSaga';
 
-export const makeStore = () => {
-  const sagaMiddleware = createSagaMiddleware();
-
-  const store = configureStore({
-    middleware: (getDefaultMiddleware) =>
-      getDefaultMiddleware({ thunk: false }).concat(sagaMiddleware),
-    reducer: rootReducer,
-  });
-
-  sagaMiddleware.run(rootSaga);
-  return store;
-};
+export const makeStore = () => configureStore({ reducer: rootReducer });
 
 export const wrapper = createWrapper(makeStore);
