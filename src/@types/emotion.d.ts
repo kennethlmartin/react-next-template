e8cd8f80de74efe795696879ef2b166a3e361821
Kenneth Martin <developer@kennethmartin.me>
2021-10-16 17:28:25 -0400

    refactor: update eslint configuration

--- src/@types/emotion.d.ts
+++ src/@types/emotion.d.ts
@@ -1,5 +1,5 @@
 import '@emotion/react';
-import { Theme as AppTheme } from 'src/themes';
+import type { Theme as AppTheme } from 'src/themes';
 
 declare module '@emotion/react' {
   export interface Theme extends AppTheme {}
